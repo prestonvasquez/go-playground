33eea47ce2769f4f5615e3a5fa35c58050526640
Preston Vasquez <prestonvasquez@icloud.com>
2026-05-27 14:48:36 -0600

    continue fixing example

--- require_deref_test.go
+++ require_deref_test.go
@@ -14,12 +14,29 @@ func (c *trCursor) close() error {
 	return c.err
 }
 
-func TestRequire_Deref(t *testing.T) {
+func TestRequire_Defer(t *testing.T) {
 	var tcr *trCursor
 
 	if tcr != nil {
-		defer func() { require.NoError(t, tcr.close()) }()
+		defer func() {
+			t.Log("closing cursor")
+			require.NoError(t, tcr.close())
+		}()
 	}
 
 	require.Nil(t, tcr)
+}
+
+func TestRequire_NoDefer(t *testing.T) {
+	var tcr *trCursor
+
+	require.Nil(t, tcr)
+	require.True(t, false)
+
+	if tcr != nil {
+		defer func() {
+			t.Log("closing cursor")
+			require.NoError(t, tcr.close())
+		}()
+	}
 }
